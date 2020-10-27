b7bebd5921c9882d079c46cb2187b21434fc6442
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-27 14:54:00 -0700

    Finished JS. Seems to be functioning as intended.
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
   $("form#userLuck").submit(function(event) {
+    // let int 
+    // let luckMeter = 0;
+    let total = 0
+
     event.preventDefault();
     $("input:checkbox[name=luck]:checked").each(function() {
-      const luckMeter = $(this).val();
-      console.log(luckMeter);
-  
+      let int 
+      let luckMeter = 0;
+      
+      int = $(this).val();
+      luckMeter = parseInt(int);
+      total += luckMeter;
     });
+    console.log(total);
+    if(total < -2){
+      $("#badLuck").show()
+    } else if(total > -2 && total < 2){
+      $("#noLuck").show()
+    } else if (total > 2){
+      $("#goodLuck").show()
+    }
   });
 });
